4986d793f923dc27688afc019bbfd1fd932125fc
richardsuan <richard.suan@correo.usa.edu.co>
2021-02-26 18:19:22 -0500

    base de datos trabajando

--- src/main/java/com/credibanco/assessment/library/dto/dotautor.java
+++ src/main/java/com/credibanco/assessment/library/dto/dotautor.java
@@ -1,5 +1,15 @@
 package com.credibanco.assessment.library.dto;
+import java.io.Serializable;
 
-public class dotautor {
-
+import com.credibanco.assessment.library.model.libro;
+public class dotautor implements Serializable{
+	
+	
+	private String nombre;
+	private String  fecha_nacimiento;
+	private String lugar_nacimiento;
+	private String correo;
+	//esto se hace para hacer las relaciones 
+	private String  libros_escritos;
+	
 }
