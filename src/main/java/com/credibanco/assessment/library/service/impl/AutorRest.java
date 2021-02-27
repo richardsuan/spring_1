6558ca910c0275ad6b96ae8521a02d49bc63da2e
richardsuan <richard.suan@correo.usa.edu.co>
2021-02-27 17:25:26 -0500

    se hace el dot para autores y todo esta funcional

--- src/main/java/com/credibanco/assessment/library/service/impl/AutorRest.java
+++ src/main/java/com/credibanco/assessment/library/service/impl/AutorRest.java
@@ -3,6 +3,8 @@ package com.credibanco.assessment.library.service.impl;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +14,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.credibanco.assessment.library.dto.dotautor;
 import com.credibanco.assessment.library.model.autor;
+import com.credibanco.assessment.library.model.libro;
 import com.credibanco.assessment.library.services.autor_servi;
+import com.credibanco.assessment.library.services.libro_servi;
+
+import javassist.runtime.DotClass;
 
 
 @RestController
 public class AutorRest {
 	@Autowired
 	private autor_servi Autor_servi;
+	private libro_servi Libro_servi;
 	@RequestMapping("/autores/")//listar autores
 	@GetMapping	
 	private ResponseEntity <List<autor>> getAllautores(){//este metodo trae a todos los autores que estan en la base de datos
@@ -30,9 +39,19 @@ public class AutorRest {
 	//
 	@RequestMapping("/autor/buscar/")//buscar por nombre 
 	@GetMapping
-	private ResponseEntity <List<autor>> getoneAutor(@RequestBody autor Autor){
+	private ResponseEntity<dotautor> getoneAutor(@RequestBody autor Autor){
 		
-		return ResponseEntity.ok(Autor_servi.findAllByName(Autor.getNombre()));//cambiar
+		//libro  libros = (libro) Libro_servi.findbookAutor(Autor.getId());
+		dotautor dot = new dotautor();
+		dot.setNombre(Autor.getNombre());
+		dot.setCorreo(Autor.getCorreo());
+		dot.setLugar_nacimiento(Autor.getLugar_nacimiento());
+		dot.setFecha_nacimiento(Autor.getFecha_nacimiento());
+		dot.setLibros_escritos(null);
+		
+		//CustomerDTO dto = new CustomerDTO();
+		//return ResponseEntity.ok(Autor_servi.findAllByName(Autor.getNombre()));//cambiar
+		return ResponseEntity.ok(dot);
 		
 	}
 	
@@ -40,13 +59,13 @@ public class AutorRest {
 	@RequestMapping("/autor/agregar/")
 	@PostMapping
 	private ResponseEntity<autor> saveAutor(@RequestBody autor Autor){//sirve para guardarlas personas y la anotacion sirve para hacer que el body sea obligatorio
-		Autor.setNombre(Autor.getNombre().toUpperCase());
-		autor Autorguardado = Autor_servi.save(Autor);
-		System.out.println("hicieron post");
+		
 		try {
+			Autor.setNombre(Autor.getNombre().toUpperCase());
+			autor Autorguardado = Autor_servi.save(Autor);
+			System.out.println("hicieron post");
 			
-			
-			return  ResponseEntity.created(new URI("/autor/"+Autorguardado.getId())).body(Autorguardado);
+			return  ResponseEntity.created(new URI("/autor/agregar/"+Autorguardado.getId())).body(Autorguardado);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
