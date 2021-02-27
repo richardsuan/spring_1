fd9ed07c9cd5c7b74cd403adc3f928e54d51f204
richardsuan <richard.suan@correo.usa.edu.co>
2021-02-27 12:16:46 -0500

    rest
    se crea el metodo post para agragar un autor

--- src/main/java/com/credibanco/assessment/library/service/impl/AutorRest.java
+++ src/main/java/com/credibanco/assessment/library/service/impl/AutorRest.java
@@ -1,9 +1,14 @@
 package com.credibanco.assessment.library.service.impl;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.credibanco.assessment.library.model.autor;
@@ -16,7 +21,20 @@ public class AutorRest {
 	@Autowired
 	private autor_servi Autor_servi;
 	@GetMapping
-	private ResponseEntity <List<autor>> getAllautores(){
+	private ResponseEntity <List<autor>> getAllautores(){//este metodo trae a todos los autores que estan en la base de datos
 		return ResponseEntity.ok(Autor_servi.findAll());
 	}
+	@PostMapping
+	private ResponseEntity<autor> saveAutor(@RequestBody autor Autor){//sirve para guardarlas personas y la anotacion sirve para hacer que el body sea obligatorio
+		autor Autorguardado = Autor_servi.save(Autor);
+		try {
+			return  ResponseEntity.created(new URI("/autor/"+Autorguardado.getId())).body(Autorguardado);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		
+	}
+	
 }
+
