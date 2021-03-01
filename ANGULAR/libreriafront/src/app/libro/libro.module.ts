8d886e88e851d088d76685be2b70b248f0467df6
richardsuan <richard.suan@correo.usa.edu.co>
2021-03-01 11:10:40 -0500

    conexion de frontend con backend para agregar un usuario completa

--- ANGULAR/libreriafront/src/app/libro/libro.module.ts
+++ ANGULAR/libreriafront/src/app/libro/libro.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LibrocontrolComponent } from './librocontrol/librocontrol.component';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [LibrocontrolComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,    
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+
   ],
   exports:[LibrocontrolComponent]
 })
--- ANGULAR/libreriafront/src/app/libro/librocontrol/librocontrol.component.ts
+++ ANGULAR/libreriafront/src/app/libro/librocontrol/librocontrol.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AutorService } from 'src/app/services/autor/autor.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-librocontrol',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./librocontrol.component.css']
 })
 export class LibrocontrolComponent implements OnInit {
-
-  constructor() { }
+  autorForm!: FormGroup;
+  constructor(
+    public fb: FormBuilder,
+    public autorService:AutorService
+  ) { }
 
   ngOnInit(): void {
+    this.autorForm = this.fb.group({
+      d_identidad : ['', Validators.required],
+      nombre : ['', Validators.required],
+      fecha_nacimiento : ['', Validators.required],
+      lugar_nacimiento : ['', Validators.required],
+      correo : ['', Validators.required],
+      pais : ['', Validators.required],
+      libros_escritos : ['', Validators.required]
+    })
+  }
+  guardar(): void {
+      this.autorService.saveAutor(this.autorForm.value).subscribe(resp => {
+      //this.autorForm.reset();
+     // this.personas=this.personas.filter(persona=> resp.id!==persona.id);
+      //this.personas.push(resp);
+    },
+      error => { console.error(error) }
+    )
   }
 
 }
