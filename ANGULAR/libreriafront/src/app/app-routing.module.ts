7073c61a5ee47c09aa624988e0fada5b28d2e4b6
richardsuan <richard.suan@correo.usa.edu.co>
2021-03-02 10:16:23 -0500

    se crea y esta funcional la barra de navegacion

--- ANGULAR/libreriafront/src/app/app-routing.module.ts
+++ ANGULAR/libreriafront/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AutorcontrolComponent } from './autor/autorcontrol/autorcontrol.component';
+import { EditorialcontrolComponent } from './editorial/editorialcontrol/editorialcontrol.component';
+import { LibrocontrolComponent } from './libro/librocontrol/librocontrol.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component:AppComponent},
+  { path: 'autor', component:AutorcontrolComponent},
+  { path: 'editorial', component:EditorialcontrolComponent},
+  { path: 'libro', component:LibrocontrolComponent},
+  { path: '**', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
